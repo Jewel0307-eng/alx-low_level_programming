4b8dc99851c6f9d8a5b8e7fccea010dd96795824
Jewel0307-eng <jewelduah09@gmail.com>
2023-08-14 06:20:29 -0700

    working

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,12 +1,7 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef PI_H
+#define PI_H
 
-/*
- * File: 0-object_like_macro.h
- * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
- */
-
-#define SIZE 1024
+#define PI 3.14159265359
 
 #endif
+
