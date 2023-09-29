0fe726aab3aee94c871dd6fe57707c0cbedec61d
Jewel Duah <jewelduah09@gmail.com>
2023-09-29 00:05:40 +0000

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,10 +2,6 @@
 
 /**
  * flip_bits - returns the number of bits you would
- * need to flip to get from one number to another
- * @n: number one.
- * @m: number two.
- *
  * Return: number of bits.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
